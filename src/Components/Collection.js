5a8c003d50685ec40a016ef76df85c999114408a
Asish K Antony <asish.antony@cmmtechnologies.in>
2020-07-27 18:11:16 +0530

    Added PWA and some other

---
+++ src/Components/Collection.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Alert, Container } from 'react-bootstrap'
+
+function Collection() {
+    return (
+        <div>
+           <Container >
+               
+           </Container>
+        </div>
+    )
+}
+
+export default Collection
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import { Alert } from 'react-bootstrap'
+
+function Home() {
+    return (
+        <div>
+            <Alert variant='secondary' >Asish</Alert>
+        </div>
+    )
+}
+
+export default Home
