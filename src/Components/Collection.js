4ab2b0156cdfe3b096586c51402af46295a47646
Asish K Antony <asish.antony@cmmtechnologies.in>
2020-07-28 01:06:13 +0530

    New logo and some corrections

--- src/Components/Collection.js
+++ src/Components/Collection.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Alert, Container } from 'react-bootstrap'
+import { Alert, Container, Jumbotron } from 'react-bootstrap'
 
 function Collection() {
     return (
         <div>
            <Container >
                <Alert variant='secondary' >This is the Collection Page</Alert>
+               <Jumbotron>This is the content of the collection page</Jumbotron>
            </Container>
         </div>
     )
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 import {Switch,Route} from 'react-router-dom'
 import Collection from './Collection'
 import Home from './Home'
@@ -17,13 +17,13 @@ function Header() {
                     <Nav className="mr-auto">
                         <Nav.Link href="/home">Home</Nav.Link>
                         <Nav.Link href="/collection">Collection</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
